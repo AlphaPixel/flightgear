877b316199d6792089d75f83d170e0beb00b440d
John W. Terrell <john@alphapixel.com>
2021-12-07 18:12:38 -0800

    More unit testing and fixes.

--- src/Network/OpenDIS/Frame.cxx
+++ src/Network/OpenDIS/Frame.cxx
@@ -13,18 +13,19 @@ namespace
     class UnitTest
     {
     public:
+        // (https://apps.dtic.mil/sti/pdfs/ADA484864.pdf)
+        const double adelaide_latitude_radians = osg::DegreesToRadians(-34.9);
+        const double adelaide_longitude_radians = osg::DegreesToRadians(138.5);
+
         UnitTest()
         {
             testNED();
             testDISOrientation();
+            testFrameDelta();
         }
 
         void testNED()
         {
-            // (https://apps.dtic.mil/sti/pdfs/ADA484864.pdf
-            const double adelaide_latitude_radians = osg::DegreesToRadians(-34.9);
-            const double adelaide_longitude_radians = osg::DegreesToRadians(138.5);
-
             Frame adelaide = Frame::fromLatLon(Angle::fromRadians(adelaide_latitude_radians), Angle::fromRadians(adelaide_longitude_radians));
 
             // (https://apps.dtic.mil/sti/pdfs/ADA484864.pdf - figure 4.8)
@@ -43,11 +44,38 @@ namespace
 
         void testDISOrientation()
         {
+            // (https://apps.dtic.mil/sti/pdfs/ADA484864.pdf - figure 5 for help understanding the below orientation values)
+
             // Start with a known frame at the prime meridian and equator
             const Frame base = Frame::fromLatLon(Angle::fromRadians(0), Angle::fromRadians(0));
 
-            // Create a simple oriention with a single rotation in Psi() - around the north pole in ECEF
+            // Create a simple orientations and verify their values.
             DIS::Orientation o;
+
+            {
+                o.setPsi(0);
+                o.setTheta(0);
+                o.setPhi(0);
+
+                auto check = base;
+                check.rotate(o);
+
+                // Check N
+                assert(is_close_thousandth(check.GetXAxis().x(),  0.0));
+                assert(is_close_thousandth(check.GetXAxis().y(),  0.0));
+                assert(is_close_thousandth(check.GetXAxis().z(),  1.0));
+
+                // Check E
+                assert(is_close_thousandth(check.GetYAxis().x(),  0.0));
+                assert(is_close_thousandth(check.GetYAxis().y(),  1.0));
+                assert(is_close_thousandth(check.GetYAxis().z(),  0.0));
+
+                // Check D
+                assert(is_close_thousandth(check.GetZAxis().x(),  -1.0));
+                assert(is_close_thousandth(check.GetZAxis().y(),  0.0));
+                assert(is_close_thousandth(check.GetZAxis().z(),  0.0));
+            }
+
 
             {
                 o.setPsi(Angle::fromDegrees(90.0).inRadians());
@@ -126,12 +154,15 @@ namespace
                 o.setTheta(Angle::fromDegrees(90.0).inRadians());
                 o.setPhi(0);
 
+                // Note: the rotation for Theta above rotates around the rotated (Psi)
+                //       Y axis which now points down the old negative X axis.
+
                 auto check = base;
                 check.rotate(o);
 
                 // Check N
                 assert(is_close_thousandth(check.GetXAxis().x(),  0.0));
-                assert(is_close_thousandth(check.GetXAxis().y(), -1.0));
+                assert(is_close_thousandth(check.GetXAxis().y(),  1.0));
                 assert(is_close_thousandth(check.GetXAxis().z(),  0.0));
 
                 // Check E
@@ -142,7 +173,7 @@ namespace
                 // Check D
                 assert(is_close_thousandth(check.GetZAxis().x(),  0.0));
                 assert(is_close_thousandth(check.GetZAxis().y(),  0.0));
-                assert(is_close_thousandth(check.GetZAxis().z(), -1.0));
+                assert(is_close_thousandth(check.GetZAxis().z(),  1.0));
             }
 
             {
@@ -160,13 +191,84 @@ namespace
 
                 // Check E
                 assert(is_close_thousandth(check.GetYAxis().x(),  0.0));
-                assert(is_close_thousandth(check.GetYAxis().y(), -1.0));
+                assert(is_close_thousandth(check.GetYAxis().y(),  1.0));
                 assert(is_close_thousandth(check.GetYAxis().z(),  0.0));
 
                 // Check D
                 assert(is_close_thousandth(check.GetZAxis().x(),  0.0));
                 assert(is_close_thousandth(check.GetZAxis().y(),  0.0));
-                assert(is_close_thousandth(check.GetZAxis().z(), -1.0));
+                assert(is_close_thousandth(check.GetZAxis().z(),  1.0));
+            }
+        }
+
+        void testFrameDelta()
+        {
+            // Start with a known frame at the prime meridian and equator
+            const Frame base = Frame::fromLatLon(Angle::fromRadians(0), Angle::fromRadians(0));
+
+            // Create a simple oriention with a single rotation in Psi() - around the north pole in ECEF
+            DIS::Orientation o;
+            
+            {
+                o.setPsi(0);
+                o.setTheta(0);
+                o.setPhi(0);
+
+                auto check = base;
+                check.rotate(o);
+
+                auto eulerAngles = check - base;
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPsi()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getTheta()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPhi()).inDegrees(), 0.0));
+            }
+#if 0
+            {
+                // A change in Psi() in ECEF space at the base frame is a bank change (in NED space)
+                // of 90 degrees.
+                o.setPsi(Angle::fromDegrees(90.0).inRadians());
+                o.setTheta(0);
+                o.setPhi(0);
+
+                auto check = base;
+                check.rotate(o);
+
+                auto eulerAngles = check - base;
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPsi()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getTheta()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPhi()).inDegrees(), 90.0));
+            }
+#endif
+            {
+                // A change in Theta() in ECEF space at the base frame is a pitch change (in NED space)
+                // of 90 degrees.
+                o.setPsi(0);
+                o.setTheta(Angle::fromDegrees(90.0).inRadians());
+                o.setPhi(0);
+
+                auto check = base;
+                check.rotate(o);
+
+                auto eulerAngles = check - base;
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPsi()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getTheta()).inDegrees(), 90.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPhi()).inDegrees(), 0.0));
+            }
+
+            {
+                // A change in Phi() in ECEF space at the base frame is a heading change (in NED space)
+                // of -90 degrees.
+                o.setPsi(0);
+                o.setTheta(0);
+                o.setPhi(Angle::fromDegrees(90.0).inRadians());
+
+                auto check = base;
+                check.rotate(o);
+
+                auto eulerAngles = check - base;
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPsi()).inDegrees(), -90.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getTheta()).inDegrees(), 0.0));
+                assert(is_close_thousandth(Angle::fromRadians(eulerAngles.getPhi()).inDegrees(), 0.0));
             }
         }
     };
@@ -202,7 +304,7 @@ void Frame::rotate(const DIS::Orientation &orientation)
     ecefFrame.rotate(Angle::fromRadians(orientation.getPsi()), -ecefFrame.GetZAxis());
 
     rotate(Angle::fromRadians(orientation.getTheta()), -ecefFrame.GetYAxis());
-    ecefFrame.rotate(Angle::fromRadians(orientation.getTheta()), ecefFrame.GetYAxis());
+    ecefFrame.rotate(Angle::fromRadians(orientation.getTheta()), -ecefFrame.GetYAxis());
 
     rotate(Angle::fromRadians(orientation.getPhi()), -ecefFrame.GetXAxis());
 }
